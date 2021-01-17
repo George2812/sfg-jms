3365707d3286074a50c9cd17983894076f5d7f98
George Dialynas <dialynas1@yahoo.com>
2021-01-17 08:38:31 +0200

    added pojo

---
+++ src/main/java/guru/springframework/model/HelloWorldMessage.java
@@ -0,0 +1,20 @@
+package guru.springframework.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class HelloWorldMessage implements Serializable {
+
+    static final long serialVersionUID = 4328659172100666284L;
+    private UUID id;
+    private String message;
+}
